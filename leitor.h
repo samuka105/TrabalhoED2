8a6600d1018da1080a9eac5c1ef529ad681833df
Jean Felipe <jean.felipe@estudante.ufjf.br>
2020-12-19 16:50:34 -0300

    Criação do leitor.h

---
+++ leitor.h
@@ -0,0 +1,18 @@
+#ifndef LEITOR_H
+#define LEITOR_H
+#include <string>
+
+using namespace std;
+
+class LeitorBase
+{
+    public:
+        LeitorBase();//construtor
+        ~LeitorBase();//destrutor
+        string getpath();//pegar o caminho
+
+}
+
+
+
+#endif // LEITOR_H
